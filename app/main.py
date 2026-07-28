d9176bd372947ad995473e808bd69170db28a7f6
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2026-07-28 19:44:22 +0530

    updated frontend url

--- app/main.py
+++ app/main.py
@@ -33,6 +33,7 @@ logger.info("Application started")
 origins = [
     "http://localhost:5173",   
     "http://127.0.0.1:5173",   
+    "https://engineering-intelligence-platform-0186.onrender.com"
 ]
 
 app.add_middleware(
